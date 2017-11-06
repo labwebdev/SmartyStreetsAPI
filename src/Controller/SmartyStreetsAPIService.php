64d39e52222097a7ec56698a826d1411d8c2c065
drupwebdev <drupwebdev@3548267.no-reply.drupal.org>
2017-11-05 19:29:03 -0700

    Modified returns to be arrays.

--- src/Controller/SmartyStreetsAPIService.php
+++ src/Controller/SmartyStreetsAPIService.php
@@ -15,21 +15,10 @@ use SmartyStreets\PhpSdk\US_Street\Lookup;
 
 class SmartyStreetsAPIService {
 
-  protected $service_example_value;
+  protected $service_default_value;
 
-  /**
-   * When the service is created, set a value for the example variable.
-   */
   public function __construct() {
-    $this->service_example_value = 'SmartyStreetsAPI Service Response';
-  }
-
-  /**
-   * Return the value of the example variable.
-   */
-  public function getServiceExampleValue($testvar) {
-    return $this->service_example_value . " " . $testvar;
-    //return self::$service_example_value;
+    $this->service_default_value = 'SmartyStreetsAPI Service Response';
   }
 
   public function LookupAddress($street_address,$city,$state) {
@@ -50,27 +39,26 @@ class SmartyStreetsAPIService {
           return $this->displayResults($lookup);
       }
       catch (SmartyException $ex) {
-        return $ex->getMessage();
+        return array("valid" => 0,"error" => $ex->getMessage());
       }
       catch (\Exception $ex) {
-        return $ex->getMessage();
+        return array("valid" => 0,"error" => $ex->getMessage());
       }
-      return 'SmartyStreetsAPI Service Testing Complete.';
+      return array("valid" => 0,"error" =>"Unidentified error. Something went wrong with SmartyStreetsAPI.");
     }
 
     public function displayResults(Lookup $lookup) {
         $results = $lookup->getResult();
         if (empty($results)) {
-            return 'No candidates. This means the address is not valid.';
+            return array("valid" => 0,"error" => "No candidates. This means the address is not valid.");
         }
 
         $firstCandidate = $results[0];
-        return 'Address is valid. (There is at least one candidate)<br>
-            ZIP Code: ' . $firstCandidate->getComponents()->getZIPCode() . '<br>
-            County: ' . $firstCandidate->getMetadata()->getCountyName() . '<br>
-            Latitude: ' . $firstCandidate->getMetadata()->getLatitude() . '<br>
-            Longitude: ' . $firstCandidate->getMetadata()->getLongitude() . '<br>
-            ';
+        return array("valid" => 1,"zipcode" => $firstCandidate->getComponents()->getZIPCode(),
+            "county" => $firstCandidate->getMetadata()->getCountyName(),
+            "latitude" => $firstCandidate->getMetadata()->getLatitude(),
+            "longitude" => $firstCandidate->getMetadata()->getLongitude());
+
     }
 
 }
