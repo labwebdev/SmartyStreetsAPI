f40e844197bfbd3e70f62eb977a970970f7262b8
LiamSch <drupal@schneiderlab.com>
2017-11-29 18:26:05 -0700

    Added API call/response functionality

--- src/Controller/SmartyStreetsAPIService.php
+++ src/Controller/SmartyStreetsAPIService.php
@@ -17,13 +17,19 @@ class SmartyStreetsAPIService {
 
   protected $service_default_value;
 
+  /**
+   * When the service is created, set a value for the example variable.
+   */
   public function __construct() {
     $this->service_default_value = 'SmartyStreetsAPI Service Response';
   }
 
   public function LookupAddress($street_address,$city,$state) {
+      $config = \Drupal::config('SmartyStreetsAPI.settings');
+      $smartylogapicalls = $config->get('SmartyStreetsAPI_log_api_calls');
       $smartyauthid = \Drupal::config('SmartyStreetsAPI.settings')->get('SmartyStreetsAPI_secret_key_auth_id');
       $smartyauthtoken = \Drupal::config('SmartyStreetsAPI.settings')->get('SmartyStreetsAPI_secret_auth_token');
+      
       $staticCredentials = new StaticCredentials($smartyauthid, $smartyauthtoken);
       $client = (new ClientBuilder($staticCredentials))
 //                        ->viaProxy("http://localhost:8080", "username", "password") // uncomment this line to point to the specified proxy.
@@ -36,6 +42,14 @@ class SmartyStreetsAPIService {
 
       try {
           $client->sendLookup($lookup);
+          if($smartylogapicalls==1){
+              \Drupal::logger('SmartyStreetsAPI')->notice('API Call: @street_address, @city, @state',
+                  array(
+                      '@street_address' => $street_address,
+                      '@city' => $city,
+                      '@state' => $state,
+                  ));
+          }
           return $this->displayResults($lookup);
       }
       catch (SmartyException $ex) {
@@ -48,17 +62,33 @@ class SmartyStreetsAPIService {
     }
 
     public function displayResults(Lookup $lookup) {
+        $config = \Drupal::config('SmartyStreetsAPI.settings');
+        $smartylogapiresponses = $config->get('SmartyStreetsAPI_log_api_responses');
         $results = $lookup->getResult();
         if (empty($results)) {
+            if($smartylogapiresponses==1){
+                \Drupal::logger('SmartyStreetsAPI')->notice('API Response: @error',
+                    array(
+                        '@error' => 'No candidates. This means the address is not valid.',
+                    ));
+            }
             return array("valid" => 0,"error" => "No candidates. This means the address is not valid.");
         }
 
         $firstCandidate = $results[0];
+        if($smartylogapiresponses==1){
+            \Drupal::logger('SmartyStreetsAPI')->notice('API Response: @zipcode, @county, @latitude, @longitude',
+                array(
+                    '@zipcode' => $firstCandidate->getComponents()->getZIPCode(),
+                    '@county' => $firstCandidate->getMetadata()->getCountyName(),
+                    '@latitude' => $firstCandidate->getMetadata()->getLatitude(),
+                    '@longitude' => $firstCandidate->getMetadata()->getLongitude(),
+                ));
+        }
         return array("valid" => 1,"zipcode" => $firstCandidate->getComponents()->getZIPCode(),
             "county" => $firstCandidate->getMetadata()->getCountyName(),
             "latitude" => $firstCandidate->getMetadata()->getLatitude(),
             "longitude" => $firstCandidate->getMetadata()->getLongitude());
-
     }
 
 }
